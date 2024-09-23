e287356948cc1a99235f11984c683a1c944cce8e
kousra <mdkousarmia71@gmail.com>
2024-09-23 10:42:34 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const donetInput =document.getElementById('donetInput');
+const donetBtnEl1 = document.getElementById("donetBtn1");
+const donetBtnEl2 = document.getElementById("donetBtn2");
+const donetBtnEl3 = document.getElementById("donetBtn3");
+const donationBtn = document.getElementById('donetInput')
+
+
+
+
+donetBtnEl1.addEventListener('click',function(){
+    console.log('clicked');
+    
+});
+donetBtnEl2.addEventListener('click',function(){
+    console.log('clicked');
+    
+});
+donetBtnEl3.addEventListener('click',function(){
+    console.log('clicked');
+    
+});
+donetInput.addEventListener('click', function(){
+
+})
